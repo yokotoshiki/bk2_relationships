044b2b24f40eeec123ca8394a1f5553d0a168d76
Toshiki Yoko <66193942+yokotoshiki@users.noreply.github.com>
2020-07-09 14:41:49 +0900

    検索機能　update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
   def search
       @user_or_book = params[:option]
       @how_search = params[:choice]
-      if@user_or_book = "1"
+      if@user_or_book == "1"
         @users = User.search(params[:search], @user_or_book, @how_search)
       else
         @books = Book.search(params[:search], @user_or_book, @how_search)
--- app/models/book.rb
+++ app/models/book.rb
@@ -7,13 +7,13 @@ class Book < ApplicationRecord
 
   def Book.search(search, user_or_book, how_search)
        if how_search == "1"
-          Book.where(['title LIKE ?' , "#{search}"])
+          Book.where(['title LIKE ?', "#{search}"])
        elsif how_search == "2"
-          Book.where(['title LIKE ?' , "#{search}%"])
+          Book.where(['title LIKE ?', "#{search}%"])
        elsif how_search == "3"
-          Book.where(['title LIKE ?' , "%#{search}"])
+          Book.where(['title LIKE ?', "%#{search}"])
         elsif how_search == "4"
-          Book.where(['title LIKE ?' , "%#{search}%"])
+          Book.where(['title LIKE ?', "%#{search}%"])
     else
       Book.all
    end
